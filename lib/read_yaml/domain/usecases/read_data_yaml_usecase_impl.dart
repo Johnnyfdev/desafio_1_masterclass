fb94761459f022976427ce488affa8c3c2f42317
JohnnyFdev <johnnyfreirejh@hotmail.com>
2022-12-16 09:04:27 -0300

    fix name Usecase

--- lib/read_yaml/domain/usecases/read_data_yaml_usecase_impl.dart
+++ lib/read_yaml/domain/usecases/read_data_yaml_usecase_impl.dart
@@ -1,6 +1,6 @@
 import '../../infra/repositories/yaml_repository_impl.dart';
 import '../repositories/yaml_repository.dart';
-import 'get_data_yaml_usecase.dart';
+import 'read_data_yaml_usecase.dart';
 
 class GetDataYamlUseCaseImpl implements GetDataYamlUseCase {
   // FIXME: CHANGE TO DI
--- test/usecase_test.dart
+++ test/usecase_test.dart
@@ -1,5 +1,5 @@
 import 'package:desafio_1_masterclass/read_yaml/domain/repositories/yaml_repository.dart';
-import 'package:desafio_1_masterclass/read_yaml/domain/usecases/get_data_yaml_usecase.dart';
+import 'package:desafio_1_masterclass/read_yaml/domain/usecases/read_data_yaml_usecase.dart';
 import 'package:desafio_1_masterclass/read_yaml/domain/usecases/read_data_yaml_usecase_impl.dart';
 import 'package:desafio_1_masterclass/read_yaml/infra/adapters/student_adapter.dart';
 
