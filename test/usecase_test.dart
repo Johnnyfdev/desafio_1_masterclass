e65b55f438e318886fbe0729dd410610ec6a746b
JohnnyFdev <johnnyfreirejh@hotmail.com>
2022-12-16 09:15:24 -0300

    refactor tests

--- test/usecase_test.dart
+++ test/usecase_test.dart
@@ -3,28 +3,28 @@ import 'package:desafio_1_masterclass/read_yaml/domain/repositories/yaml_reposit
 import 'package:desafio_1_masterclass/read_yaml/domain/usecases/read_data_yaml_usecase.dart';
 import 'package:desafio_1_masterclass/read_yaml/domain/usecases/read_data_yaml_usecase_impl.dart';
 import 'package:desafio_1_masterclass/read_yaml/external/datasources/local_file/read_local_file_datasource_impl.dart';
-import 'package:desafio_1_masterclass/read_yaml/infra/adapters/student_adapter.dart';
 import 'package:desafio_1_masterclass/read_yaml/infra/repositories/yaml_repository_impl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  final YamlRepository repository = YamlRepositoryImpl(
+    ReadLocalFileDatasourceImpl(),
+    YamlDtoImpl(),
+  );
+
   setUpAll(() {
     WidgetsFlutterBinding.ensureInitialized();
   });
-  group('usecase ...', () {
-    test('get Yaml', () async {
-      final YamlRepository repository = YamlRepositoryImpl(
-        ReadLocalFileDatasourceImpl(),
-        YamlDtoImpl(),
-      );
+  group('READ YAML DATA FILE', () {
+    test('Must return a Map from reading YAML File', () async {
       final GetDataYamlUseCase getDataYamlUsecase =
           GetDataYamlUseCaseImpl(repository);
-
       var data = await getDataYamlUsecase();
-      var entity = StudentAdapter.mapToEntity(data);
 
-      debugPrint(entity.toString());
+      debugPrint(data.toString());
+
+      expect(data, isA<Map>());
     });
   });
 }
