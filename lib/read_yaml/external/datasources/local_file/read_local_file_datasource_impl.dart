66a24cc81ebc0a5098ca7f3cd4ddde71b65f894d
JohnnyFdev <johnnyfreirejh@hotmail.com>
2022-12-16 08:36:07 -0300

    refactor datasource

--- lib/read_yaml/external/datasources/local_file/read_local_file_datasource_impl.dart
+++ lib/read_yaml/external/datasources/local_file/read_local_file_datasource_impl.dart
@@ -5,7 +5,8 @@ import '../../../infra/datasources/local_file/read_local_file_datasource.dart';
 class ReadLocalFileDatasourceImpl implements ReadLocalYamlFileDatasource {
   @override
   Future<String> call() async {
-    var data = await rootBundle.loadString('assets/yaml/teste.yaml');
+    String path = 'assets/yaml/teste.yaml';
+    var data = await rootBundle.loadString(path);
     return data;
   }
 }
