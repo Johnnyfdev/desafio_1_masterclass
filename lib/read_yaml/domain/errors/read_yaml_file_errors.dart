b20a8a6006468607a81b1f1b7aa31a5ec085f2a5
JohnnyFdev <johnnyfreirejh@hotmail.com>
2022-12-16 09:34:01 -0300

    implements errors

---
+++ lib/read_yaml/domain/errors/read_yaml_file_errors.dart
@@ -0,0 +1,9 @@
+import '../../../core/errors/desafio_error.dart';
+
+abstract class ReadYamlFileErrors extends DesafioErrors {
+  ReadYamlFileErrors(String message) : super(message: message);
+}
+
+class UnknowReadYamlFileError extends ReadYamlFileErrors {
+  UnknowReadYamlFileError() : super('Unknow Read Yaml File Error');
+}
